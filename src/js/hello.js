f294c860f9f7518bbaa3bb766dd2e48282a93cf2
Pahnki Kim <pahnki.kim.hanbit@gmail.com>
2016-12-12 15:46:57 +0900

    최초 커밋

---
+++ src/js/hello.js
@@ -0,0 +1,30 @@
+$(function() {
+    $(".logo").css("border-color", "red");
+
+    // JSON (JavaScript Object Notation)
+    var classRoom = {
+        name: "5강의실",
+        location: "거구장 지하 1층",
+        students: [{
+            name: "이상윤",
+            gender: "남"
+        }, {
+            name: "조영욱",
+            gender: "남"
+        }, {
+            name: "백수현",
+            gender: "여"
+        }]
+    };
+
+    var printStudents = function() {
+        for (var i=0;i<classRoom.students.length;i++) {
+            var student = classRoom.students[i];
+            console.log(student.name + " (" + student.gender + ")", "abcd", "hello");
+        }
+
+        return 4;
+    };
+
+    console.log(printStudents());
+});
