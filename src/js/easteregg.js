868c598d878dbce7fca8e301151bac392e507973
Pahnki Kim <pahnki.kim.hanbit@gmail.com>
2016-12-23 11:30:16 +0900

    update santa sprite

--- src/js/easteregg.js
+++ src/js/easteregg.js
@@ -2,20 +2,32 @@ define([
     "jquery"
 ], function () {
     var Key = {
+        A: 65, W: 87, D: 68, S: 83,
         LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40,
         PLUS: 107, MINUS: 109
     };
 
+    var Direction = {
+        LEFT: -1,
+        RIGHT: 1
+    };
+
     var Const = {
-        INTERVAL: 30,
+        INTERVAL: 50,
         MOVE: 3, MIN_MOVE: 3, MAX_MOVE: 21
     };
 
     var inputKeyCode = Key.RIGHT;
 
+    var Frame = {
+        current: 0,
+        frames: 4,
+        direction: Direction.RIGHT
+    };
+
     function initBlock() {
-        $("body").append("<div id='block' style='width:30px;height:30px;" +
-            "background-color: red;z-index: 100;position: fixed;" +
+        $("body").append("<div id='block' style='width:72px;height:72px;" +
+            "background-image: url(img/santa.png);z-index: 100;position: fixed;" +
             "top:0px;left:0px;'></div>");
     }
 
@@ -41,9 +53,11 @@ define([
 
         if (inputKeyCode === Key.LEFT && newLeft < 0) {
             inputKeyCode = Key.RIGHT;
+            Frame.direction = Direction.RIGHT;
         }
         else if (inputKeyCode === Key.RIGHT && newLeft > $(window).width()) {
             inputKeyCode = Key.LEFT;
+            Frame.direction = Direction.LEFT;
         }
         else if (inputKeyCode === Key.UP && newTop < 0) {
             inputKeyCode = Key.DOWN;
@@ -52,20 +66,49 @@ define([
             inputKeyCode = Key.UP;
         }
 
+        $("#block").css("background-position", "-" + (Frame.current * 72) + "px 0");
+        $("#block").css("transform", "scaleX(" + Frame.direction + ")");
+
         $("#block").css("left", newLeft + "px");
         $("#block").css("top", newTop + "px");
 
+        Frame.current++;
+
+        if (Frame.current >= Frame.frames) {
+            Frame.current = 0;
+        }
+
         setTimeout(moveBlock, Const.INTERVAL);
+    }
+
+    function updateKeyCode(event) {
+        event.preventDefault();
+        inputKeyCode = event.keyCode;
     }
 
     $(document).on("keydown", function(event) {
         switch (event.keyCode) {
             case Key.LEFT:
+            case Key.A:
+                Frame.direction = Direction.LEFT;
+                event.keyCode = Key.LEFT;
+                updateKeyCode(event);
+                break;
             case Key.UP:
+            case Key.W:
+                event.keyCode = Key.UP;
+                updateKeyCode(event);
+                break;
             case Key.RIGHT:
+            case Key.D:
+                event.keyCode = Key.RIGHT;
+                Frame.direction = Direction.RIGHT;
+                updateKeyCode(event);
+                break;
             case Key.DOWN:
-                event.preventDefault();
-                inputKeyCode = event.keyCode;
+            case Key.S:
+                event.keyCode = Key.DOWN;
+                updateKeyCode(event);
                 break;
             case Key.PLUS:
                 Const.MOVE = Math.min(Const.MOVE + 3, Const.MAX_MOVE);
